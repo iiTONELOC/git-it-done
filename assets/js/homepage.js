93d4fcdf5d100e3284505568bc21ce4393116a5f
iiTONELOC <anthonytropeano@me.com>
2021-01-26 13:45:30 -0500

    added error response handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -23,17 +23,29 @@ var getUserRepos= function(user){
     var apiUrl= "https://api.github.com/users/" + user +"/repos"
 
     //make a request to the url
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
+    fetch(apiUrl)
+    .then(function(response) {
+    // request was successful
+    if (response.ok) {
+        response.json().then(function(data) {
             displayRepos(data, user);
-        });
+        })
+        } else {
+        alert("Error: " + response.statusText);
+        }
+    }).catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
     });
-
 };
 userFormEl.addEventListener('submit', formSubmitHandler);
 
 //display data from API request on page
 var displayRepos = function(repos, searchTerm){
+    if(repos.length ===0){
+        repoContainerEl.textContent= "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     //clear old content
